42281894c65c00dea227114f27010c19b164eb2d
RizkyMardiyansyah <rizkymardiyansyah23@gmail.com>
2025-01-07 22:23:22 +0700

    update

--- resources/views/domain-check.blade.php
+++ resources/views/domain-check.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="img/favicon.ico">
+    <meta name="title" content="Mitra Adidaya Rekayasa Digital">
     <title>Mitra Adidaya Rekayasa Digital</title>
     {{-- <title>{{ $sitename }}</title> --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
