5cd3e011c472a80da857b7fcab7036d5b9382c38
RizkyMardiyansyah <rizkymardiyansyah23@gmail.com>
2025-01-13 16:19:07 +0700

    update

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -140,13 +140,11 @@ class DomainController extends Controller
         }
 
         // Set your Merchant Server Key
-        \Midtrans\Config::$serverKey = config('midtrans.serverKey');
-        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$serverKey =config('midtrans.serverKey');
         \Midtrans\Config::$isProduction = false;
-        // Set sanitization on (default)
-        \Midtrans\Config::$isSanitized = true;
-        // Set 3DS transaction for credit card to true
-        \Midtrans\Config::$is3ds = true;
+        \Midtrans\Config::$isSanitized =true;
+       \Midtrans\Config::$is3ds = true;        
+        
 
         $params = array(
             'transaction_details' => array(
