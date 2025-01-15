6e68c8ba3b8db85ec268a6dbc55dbe0e6befd3c7
RizkyMardiyansyah <rizkymardiyansyah23@gmail.com>
2025-01-15 17:20:54 +0700

    up

--- resources/views/orderMail.blade.php
+++ resources/views/orderMail.blade.php
@@ -109,7 +109,7 @@
             text-decoration: none !important;
         }
         .price{
-            text-align: right;
+            margin-left: auto;
         }
     </style>
 </head>
@@ -138,7 +138,7 @@
                         </div>
                         <span class="cart-des" id="selected-domain">{{ $data['domain'] }}</span>
                         <div style="display: flex; justify-content: space-between;">
-                            <strong>Website Template:</strong>
+                            <strong>Website Template</strong>
                             <div class="price"><span>Rp. {{ $data['template_cost'] == 0 ? 'Rp. 0' : 'Rp. ' . number_format($data['templateCost'], 2, ',', '.') }}</span></div>
                         </div>
                         <span class="cart-des" id="selected-template">{{ $template['title'] }}</span>
