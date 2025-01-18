109efedf489b4c5f72280ad5bdd8f5e54346c370
RizkyMardiyansyah <rizkymardiyansyah23@gmail.com>
2025-01-18 13:53:06 +0700

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -236,8 +236,8 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 <script> 
-    sessionStorage.clear();
-     $(document).ready(function() {
+    sessionStorage.clear();  
+        $(document).ready(function() {
 
         // Event untuk form pencarian
         $('#searchTemplateForm').on('submit', function(e) {
@@ -278,30 +278,26 @@
                         response.templates.forEach(template => {
                             const imageUrl = `{{ url('storage') }}/${template.image}`;
                             $('#templateContainer').append(`
-                            
-
-                            <div class="card webtemplate col-lg-4 col-md-6 col-12">
-                                <a href="${template.link}" target="_blank">
-                                    <img src="${imageUrl}" alt="${template.title}">
-                                </a>
-                                <div class="d-flex" style="padding: 0px">
-                                    {{-- <div class="card-title">${template.title}</div> --}}
-                                    <div class="card-title">
-                                        <a href="${template.link}" target="_blank" style="text-decoration: none; color: inherit;">
-                                            ${template.title}
-                                        </a>
+                                <div class="card webtemplate col-lg-4 col-md-6 col-12">
+                                    <a href="${template.link}" target="_blank">
+                                        <img src="${imageUrl}" alt="${template.title}">
+                                    </a>
+                                    <div class="d-flex" style="padding: 0px">
+                                        {{-- <div class="card-title">${template.title}</div> --}}
+                                        <div class="card-title">
+                                            <a href="${template.link}" target="_blank" style="text-decoration: none; color: inherit;">
+                                                ${template.title}
+                                            </a>
+                                        </div>
+                                        <div class="d-flex" style="margin-left: auto">
+                                            <a href="#" class="view select d-flex align-items-center justify-content-center" data-template-id="${template.id}" data-template-type="${template.type}" data-template-title="${template.title}" data-bs-toggle="tooltip" title="Pilih Template"><i class="fas fa-check"></i></a>
+                                            <a href="${template.link}" target="_blank" class="view d-flex align-items-center justify-content-center" data-bs-toggle="tooltip" title="Live Preview"><i class="fas fa-eye"></i></a>
+                                        </div>
                                     </div>
-                                    <div class="d-flex" style="margin-left: auto">
-                                        <a href="#" class="view select d-flex align-items-center justify-content-center" data-template-id="${template.id}" data-template-type="${template.type}" data-template-title="${template.title}" data-bs-toggle="tooltip" title="Pilih Template"><i class="fas fa-check"></i></a>
-                                        <a href="${template.link}" target="_blank" class="view d-flex align-items-center justify-content-center" data-bs-toggle="tooltip" title="Live Preview"><i class="fas fa-eye"></i></a>
+                                    <div class="" style="padding: 0px; Opacity:50%; font-size:14px; margin-top:auto">       
+                                        <div class="card-title"> <i class="fa fa-shopping-bag me-2"></i>${template.total_pembelian} Purchased</div>
                                     </div>
                                 </div>
-                                <div class="" style="padding: 0px; Opacity:50%; font-size:14px; margin-top:auto">       
-                                    <div class="card-title"> <i class="fa fa-shopping-bag me-2"></i>${template.total_pembelian} Purchased</div>
-                                </div>
-                                
-                            </div>
-
                             `);
                         });
                     } else {
