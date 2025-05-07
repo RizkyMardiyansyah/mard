49627ff5cd5e6e6061164c054d0a9312de6ee991
RizkyMardiyansyah <rizkymardiyansyah23@gmail.com>
2025-05-07 09:19:23 +0700

    update api midtrans

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -172,9 +172,9 @@ public function template()
 
         // Midtrans
         \Midtrans\Config::$serverKey = config('midtrans.serverKey');
-        \Midtrans\Config::$isProduction = false;
-        \Midtrans\Config::$isSanitized = true;
-        \Midtrans\Config::$is3ds = true;
+        \Midtrans\Config::$isProduction = config('midtrans.isProduction');
+        \Midtrans\Config::$isSanitized = config('midtrans.isSanitized');
+        \Midtrans\Config::$is3ds = config('midtrans.is3ds');
 
         $params = [
             'transaction_details' => [
